51f1dc7ff026c1300dfcf978e6f4ac8b2f022288
Daniilvolodin <51727111+Daniilvolodin@users.noreply.github.com>
2020-07-01 10:29:41 +1200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_error','On'); // change to 'Off' when live
+define ("DB_NAME","volodind70370");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_HOST","localhost");
+?>
