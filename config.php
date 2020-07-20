45d556d1450eaabb5e54815ac28d6fc2eb821338
Daniilvolodin <51727111+Daniilvolodin@users.noreply.github.com>
2020-07-21 11:41:02 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_error','On'); // change to 'Off' when live
+ini_set('display_error','Off'); // change to 'Off' when live
 define ("DB_NAME","volodind70370");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","");
+define("DB_USERNAME","volodind70370");
+define("DB_PASSWORD","25Culpable4");
 define("DB_HOST","localhost");
 ?>
